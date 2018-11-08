e29d961998f5f4ab4054b23825b93dac31f62432
MarcosPedraza <marcos9593@gmail.com>
2018-11-08 01:09:03 -0600

    commit inicial

---
+++ app/src/main/java/com/uaeh/garza/garzamapp/Fragments/FlotillasFragment.java
@@ -0,0 +1,31 @@
+package com.uaeh.garza.garzamapp.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.uaeh.garza.garzamapp.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FlotillasFragment extends Fragment {
+
+
+    public FlotillasFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_flotillas, container, false);
+    }
+
+}
